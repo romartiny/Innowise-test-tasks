55369536ef2944cfd75637d652c49bc132269021
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-07 00:38:27 +0300

    update

--- task1.php
+++ task1.php
@@ -1,3 +1,18 @@
 <?php
 
-?>
+class task1 {
+//    public $inputNumber;
+
+    function result( int $inputNumber ) : string {
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Less than 10'));
+    }
+
+    function main( int $inputNumber ) : string {
+        return $this->result($inputNumber);
+    }
+}
+
+$object = new task1();
+echo $object->main(12);
+
+
