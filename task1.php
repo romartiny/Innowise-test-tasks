32388fc7bb8a91594a492f5c38d17f9a04af0f5a
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-07 16:03:52 +0300

    task-1 update

--- task1.php
+++ task1.php
@@ -6,10 +6,7 @@ class task1
 {
     public function result(int $inputNumber): string
     {
-        if(is_int($inputNumber)) {
-            $ans = $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Less than 10'));
-        }
-
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Less than 10'));
     }
 
     public function main(int $inputNumber): string
