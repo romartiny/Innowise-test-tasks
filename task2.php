2684757317c4b6e3c613c51bab63d78721ad28ae
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-07 13:57:39 +0300

    task-2 update

--- task2.php
+++ task2.php
@@ -12,15 +12,15 @@ class task2
         $newDate = date('Y-m-d', strtotime($today.' + 1 year'));
         $inToday = explode('-', $newDate);
         echo $inToday[0];
-        $timeBirthday = mktime(0, 0, 0, $inDate[1], $inDate[2]);
+        $timeBirthday = mktime(0, 0, 0, $inDate[1], $inDate[2], $inDate[0]);
 
         if ($timeBirthday < $today) {
-            $nextBirthDate = mktime(0, 0, 0, $inDate[1], $inDate[2], $inToday[0]);
+            $nextBirthDate = ((mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y', strtotime($today.' + 1 year')))) - $today) / $secondsInDay;
         } else {
-            $nextBirthDate = mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y'));
+            $nextBirthDate = ((mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y')))) - $today) / $secondsInDay;
         }
 
-        return ($nextBirthDate - $today) / $secondsInDay;
+        return $nextBirthDate;
     }
 
     public function main($date): string
