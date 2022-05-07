a253d871ac53ccf30f48056716c68ffa97bb7e5f
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-07 15:57:03 +0300

    task-2 update

--- task2.php
+++ task2.php
@@ -7,19 +7,23 @@ class task2
     public function birthday($date): int
     {
         $inDate = explode('-', $date);
-        $secondsInDay = 24 * 60 * 60;
-        $today = date('Y-m-d');
-        $inToday = explode('-', $today);
-        $inYear = (int) $inToday[0] + 1;
-        $time = time();
-        $timeBirthday = mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y'));
+        if (checkdate($inDate[1], $inDate[2], $inDate[0])) {
+            $secondsInDay = 24 * 60 * 60;
+            $today = date('Y-m-d');
+            $inToday = explode('-', $today);
+            $inYear = (int) $inToday[0] + 1;
+            $time = time();
+            $timeBirthday = mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y'));
 
-        if ($timeBirthday < $time) {
-            $nextBirthDate = (mktime(0, 0, 0, $inDate[1], $inDate[2], $inYear) - $time) / $secondsInDay;
+            if ($timeBirthday < $time) {
+                $nextBirthDate = (mktime(0, 0, 0, $inDate[1], $inDate[2], $inYear) - $time) / $secondsInDay;
+            } else {
+                $nextBirthDate = mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y')) - $time / $secondsInDay;
+            }
         } else {
-            $nextBirthDate = mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y')) - $time / $secondsInDay;
+            echo 'Wrong Data';
+            exit;
         }
-
         return $nextBirthDate;
     }
 
@@ -30,4 +34,4 @@ class task2
 }
 
 $object = new Task2();
-echo $object->main('2001-03-21');
+echo $object->main('2001-013-21');
