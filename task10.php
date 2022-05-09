291040a3aa8e2df0c2681241a308eed2166549e3
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-09 21:01:20 +0300

    task-10 update

---
+++ task10.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace src;
+
+class Task10
+{
+    public function main(int $input): array|bool
+    {
+        if ($input <= 0) {
+            throw new \InvalidArgumentException();
+        }
+        $newArr[] = $input;
+        while ($input >= 2) {
+            if ($input & 1) {
+                $input = 3 * $input + 1;
+                $newArr[] = $input;
+            } else {
+                $input = $input / 2;
+                $newArr[] = $input;
+            }
+        }
+
+        return print_r($newArr);
+    }
+}
+
+//$object = new Task10();
+//echo $object->main(-1);
