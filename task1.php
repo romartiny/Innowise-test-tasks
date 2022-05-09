d00658d44b3c3a5980746da8cc5cf353bc5b5330
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-10 00:59:41 +0300

    task-1 fix

--- task1.php
+++ task1.php
@@ -6,10 +6,6 @@ class Task1
 {
     public function result(int $inputNumber): string
     {
-        if (!is_int($inputNumber)) {
-            throw new Exception('Wrong Data');
-        }
-
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Less than 10'));
     }
 
