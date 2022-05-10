557219f8dc640cce75135bb51f3d634ec7e67245
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-10 22:06:21 +0300

    task-2 fix

--- Task2.php
+++ Task2.php
@@ -4,7 +4,7 @@ namespace src;
 
 class Task2
 {
-    public function birthday(string $date): int
+    public function main(string $date): int
     {
         $inDate = explode('-', $date);
         $secondsInDay = 24 * 60 * 60;
@@ -13,27 +13,18 @@ class Task2
         $inYear = (int) $inToday[0];
         $time = time();
         $timeBirthday = mktime(0, 0, 0, $inDate[1], $inDate[2], $inDate[0]);
-
         if (!checkdate($inDate[0], $inDate[1], $inDate[2])) {
             throw new \InvalidArgumentException();
         }
-
         if ($inToday[1] === $inDate[1] && $inDate[2] === $inToday[2]) {
             return 0;
         }
-
         if ($timeBirthday < $time) {
             $nextBirthDate = (mktime(0, 0, 0, $inDate[1], $inDate[2], $inYear + 1) - $time) / $secondsInDay;
-
         } else {
             $nextBirthDate = (mktime(0, 0, 0, $inDate[1], $inDate[2], date('Y')) - $time) / $secondsInDay;
         }
 
         return round($nextBirthDate) + 1;
-    }
-
-    public function main(string $date): int
-    {
-        return $this->birthday($date);
     }
 }
