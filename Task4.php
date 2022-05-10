55c0031ecc8065d398eecd52bace049d5c27fb2e
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-10 22:03:31 +0300

    task-4 fix

--- Task4.php
+++ Task4.php
@@ -4,15 +4,11 @@ namespace src;
 
 class Task4
 {
-    public function string(string $input): string
+    public function main(string $input): string
     {
         $patterns = ['/-/', '/_/'];
         $string = ucwords(preg_replace($patterns, ' ', $input));
-        return str_replace(' ', '', $string);
-    }
 
-    public function main(string $input): string
-    {
-        return $this->string($input);
+        return str_replace(' ', '', $string);
     }
 }
