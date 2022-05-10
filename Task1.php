bba4ad11160c09a59a52a714223e11fba8a5709f
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-10 18:10:35 +0300

    task-1 fix

--- Task1.php
+++ Task1.php
@@ -4,14 +4,10 @@ namespace src;
 
 class Task1
 {
-    public function result(int $inputNumber): string
+    public function main(int $inputNumber): string
     {
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Less than 10'));
     }
-
-    public function main(int $inputNumber): string
-    {
-        return $this->result($inputNumber);
-    }
 }
+
 
