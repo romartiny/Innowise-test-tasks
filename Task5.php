e4499c5b5a09ce37e7f7220d621772be0043cd10
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-10 16:19:14 +0300

    task-5 fix

--- Task5.php
+++ Task5.php
@@ -4,30 +4,19 @@ namespace src;
 
 class Task5
 {
-    public function fib(int $n): string
+    public function main(int $n): string
     {
         if ($n <= 0) {
             throw new \InvalidArgumentException();
         }
-
-        $num = 0;
-        $n1 = 0;
-        $n2 = 1;
+        $n1 = '1';
+        $n2 = '1';
         while (mb_strlen($n2) < $n) {
-            $n3 = $n2 + $n1;
+            $n3 = bcadd($n2, $n1);
             $n1 = $n2;
             $n2 = $n3;
-            $num = $num + 1;
         }
 
-        return (string) $n2;
-    }
-
-    public function main(int $n): string
-    {
-        return $this->fib($n);
+        return $n3;
     }
 }
-
-$object = new task5();
-echo $object->main(12);
