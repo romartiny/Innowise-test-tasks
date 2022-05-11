cd5ef0089d4b1f4c82cfdbf13a06153dba5c6ae6
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-11 15:19:54 +0300

    task-9 fix

--- Task9.php
+++ Task9.php
@@ -4,7 +4,7 @@ namespace src;
 
 class Task9
 {
-    public function main(array $arr, int $number): array|bool
+    public function main(array $arr, int $number): array
     {
         for ($i = 0; $i < \count($arr); ++$i) {
             if (\count($arr) < 3 || $number <= 0 || $arr[$i] < 0) {
@@ -13,9 +13,10 @@ class Task9
         }
         $countArr = \count($arr) - 2;
         $newArr = [];
+        $resArr = [];
         for ($i = 0; $i < $countArr; ++$i) {
             if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] === $number) {
-                $newArr[] = "['0' => '{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = {$number}']";
+                $newArr[] = array("'{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = {$number}'");
             }
         }
 
