79beaf7dbf20dc32f2b1fc4bdf48bea325cfbb7b
romartiny <91370772+romartiny@users.noreply.github.com>
2022-05-11 14:26:17 +0300

    task-7 fix

--- Task7.php
+++ Task7.php
@@ -4,14 +4,13 @@ namespace src;
 
 class Task7
 {
-    public function main(array $arr, int $position): array|string
+    public function main(array $arr, int $position): array
     {
         if (empty($arr) || $position < 0 || \count($arr) <= $position) {
             throw new \InvalidArgumentException();
         }
         unset($arr[$position]);
-        $newArr = array_values($arr);
 
-        return print_r($newArr);
+        return array_values($arr);
     }
 }
