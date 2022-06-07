447ffa019731f03658f393f6bfd9bc7b13f07ab6
romartiny <91370772+romartiny@users.noreply.github.com>
2022-06-07 12:10:19 +0300

    del token

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 
 class Config
 {
-    public $token = '6e39c3909608ffdfdba86f91fee92212851f3904bffbdcc0fe7df8ef2fe56f4d';
+    public $token = '';
     public $basicUrl = 'https://gorest.co.in/public/v2/users';
 
     public function __construct()
