b64bbc9d8d497ac6f35f35174953ce316277ad56
romartiny <91370772+romartiny@users.noreply.github.com>
2022-06-14 10:52:26 +0300

    fix namespaces

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ namespace App;
 
 require_once __DIR__ . '/Controller/UserController.php';
 
-use App\UserController\UserController;
+use App\UserController\UserController as UserController;
 
 $controller = new UserController();
 $controller->init();
