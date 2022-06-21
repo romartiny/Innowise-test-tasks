d2ee15f2a9a1ba9a25bff867427d01df859eedac
Romartiny <romortiny@gmail.com>
2022-06-21 10:44:59 +0300

    fix

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -5,7 +5,6 @@ namespace App\UserModel;
 require_once __DIR__ . '/../Config/DatabaseConfig.php';
 
 use App\DatabaseConfig\DatabaseConfig;
-use Exception;
 
 class UserModel
 {
@@ -21,7 +20,7 @@ class UserModel
         try {
             $query = 'INSERT INTO users (email, first_name, last_name, password) VALUES (?,?,?,?)';
             $this->connect->prepare($query)->execute(array($email, $firstName, $lastName, $password));
-        } catch (Exception $exception) {
+        } catch (\Exception $exception) {
             die($exception->getMessage());
         }
     }
